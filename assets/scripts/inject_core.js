4454d75749b5cbb84dceddd3be39027b263cd8f8
Eridanus Sora <sora@sound.moe>
2019-03-29 13:03:29 +0800

    fix bugs

--- assets/scripts/inject_core.js
+++ assets/scripts/inject_core.js
@@ -173,7 +173,7 @@
     }
 
     const twicas = async () => {
-        const userName = location.href.match(/https:\/\/twitcasting.tv\/(.+)/);
+        const userName = location.href.match(/https:\/\/twitcasting.tv\/(.+?)(\/|$)/);
         if (userName) {
             await fetch(`https://twitcasting.tv/${userName[1]}/metastream.m3u8`);
         }
