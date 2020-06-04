f666e59c2f4afb1f7f8d3ec64d55e60f18b64061
Eridanus Sora <sora@sound.moe>
2020-06-04 09:33:42 +0800

    fix nico

--- assets/scripts/inject_core.js
+++ assets/scripts/inject_core.js
@@ -1,87 +1,108 @@
 (async () => {
     console.info(`Minyami Extractor inited.`);
-    const MINYAMI_EXTENSION_ID = navigator.userAgent.includes('Edg') ? "djgfdgeokfljmadfphgoemldapjilanp" : "cgejkofhdaffiifhcohjdbbheldkiaed";
+    // const MINYAMI_EXTENSION_ID = navigator.userAgent.includes('Edg') ? "djgfdgeokfljmadfphgoemldapjilanp" : "cgejkofhdaffiifhcohjdbbheldkiaed";
+    const MINYAMI_EXTENSION_ID = "cgejkofhdaffiifhcohjdbbheldkiaed";
 
-    let key = '';
+    let key = "";
     if (window.fetch) {
         const _fetch = fetch;
         fetch = (url, ...fargs) => {
             return new Promise((resolve, reject) => {
-                _fetch(url, ...fargs).then(async res => {
-                    resolve(res);
-                    return res.clone();
-                }).then(async r => {
-                    if (r.url.includes('m3u8')) {
-                        const responseText = await r.text();
-                        if (responseText.match(/#EXT-X-STREAM-INF/) !== null) {
-                            chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                                "type": "playlist",
-                                "content": responseText,
-                                "url": r.url,
-                                "title": document.title.replace(/[\/\*\\\:|\?<>]/ig, "")
-                            })
-                        } else {
-                            chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                                "type": "chunklist",
-                                "content": responseText,
-                                "url": r.url,
-                                "title": document.title.replace(/[\/\*\\\:|\?<>]/ig, "")
-                            })
+                _fetch(url, ...fargs)
+                    .then(async (res) => {
+                        resolve(res);
+                        return res.clone();
+                    })
+                    .then(async (r) => {
+                        if (r.url.includes("m3u8")) {
+                            const responseText = await r.text();
+                            if (
+                                responseText.match(/#EXT-X-STREAM-INF/) !== null
+                            ) {
+                                chrome.runtime.sendMessage(
+                                    MINYAMI_EXTENSION_ID,
+                                    {
+                                        type: "playlist",
+                                        content: responseText,
+                                        url: r.url,
+                                        title: document.title.replace(
+                                            /[\/\*\\\:|\?<>]/gi,
+                                            ""
+                                        ),
+                                    }
+                                );
+                            } else {
+                                chrome.runtime.sendMessage(
+                                    MINYAMI_EXTENSION_ID,
+                                    {
+                                        type: "chunklist",
+                                        content: responseText,
+                                        url: r.url,
+                                        title: document.title.replace(
+                                            /[\/\*\\\:|\?<>]/gi,
+                                            ""
+                                        ),
+                                    }
+                                );
+                            }
                         }
-                    }
-                }).catch(e => {
-                    reject(e);
-                });
-            })
-        }
+                    })
+                    .catch((e) => {
+                        reject(e);
+                    });
+            });
+        };
     }
     XMLHttpRequest.prototype._open = XMLHttpRequest.prototype.open;
-    Object.defineProperty(XMLHttpRequest.prototype, 'open', {
-        get: function () {
+    Object.defineProperty(XMLHttpRequest.prototype, "open", {
+        get: function() {
             return this._open;
         },
-        set: function (f) {
+        set: function(f) {
             this._open = new Proxy(f, {
-                apply: function (f, instance, fargs) {
+                apply: function(f, instance, fargs) {
                     listen.call(instance, ...fargs);
-                    return f.call(instance, ...fargs)
-                }
+                    return f.call(instance, ...fargs);
+                },
             });
-        }
+        },
     });
     XMLHttpRequest.prototype.open = XMLHttpRequest.prototype.open;
-    const listen = function () {
-        this.addEventListener('load', function () {
-            if (this.responseURL.includes('m3u8')) {
+    const listen = function() {
+        this.addEventListener("load", function() {
+            if (this.responseURL.includes("m3u8")) {
                 console.log(this.responseURL);
             }
-            if (this.readyState === 4 && new URL(this.responseURL).pathname.endsWith('m3u8')) {
+            if (
+                this.readyState === 4 &&
+                new URL(this.responseURL).pathname.endsWith("m3u8")
+            ) {
                 if (this.responseText.match(/#EXT-X-STREAM-INF/) !== null) {
                     chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                        "type": "playlist",
-                        "content": this.responseText,
-                        "url": this.responseURL,
-                        "title": document.title.replace(/[\/\*\\\:|\?<>]/ig, "")
-                    })
+                        type: "playlist",
+                        content: this.responseText,
+                        url: this.responseURL,
+                        title: document.title.replace(/[\/\*\\\:|\?<>]/gi, ""),
+                    });
                 } else {
                     chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                        "type": "chunklist",
-                        "content": this.responseText,
-                        "url": this.responseURL,
-                        "title": document.title.replace(/[\/\*\\\:|\?<>]/ig, "")
-                    })
+                        type: "chunklist",
+                        content: this.responseText,
+                        url: this.responseURL,
+                        title: document.title.replace(/[\/\*\\\:|\?<>]/gi, ""),
+                    });
                 }
                 // Execute after m3u8 loads
                 switch (location.host) {
-                    case 'live2.nicovideo.jp': {
+                    case "live2.nicovideo.jp": {
                         nico();
                         break;
                     }
-                    case 'www.dmm.com': {
+                    case "www.dmm.com": {
                         dmm(this);
                         break;
                     }
-                    case 'www.dmm.co.jp': {
+                    case "www.dmm.co.jp": {
                         dmm_r18(this);
                         break;
                     }
@@ -89,30 +110,30 @@
             }
             // Execute when first AJAX request finished
             switch (location.host) {
-                case 'www.dmm.com': {
+                case "www.dmm.com": {
                     dmm(this);
                     break;
                 }
-                case 'www.dmm.co.jp': {
+                case "www.dmm.co.jp": {
                     dmm_r18(this);
                     break;
                 }
-                case 'www.360ch.tv': {
+                case "www.360ch.tv": {
                     ch360(this);
                     break;
                 }
-                case 'cas.nicovideo.jp': {
+                case "cas.nicovideo.jp": {
                     nicocas(this);
                 }
-                case 'hibiki-radio.jp': {
+                case "hibiki-radio.jp": {
                     hibiki(this);
                 }
-                case 'www.showroom-live.com': {
+                case "www.showroom-live.com": {
                     showroom(this);
                 }
             }
         });
-    }
+    };
     /**
      * Site Scripts
      */
@@ -120,166 +141,223 @@
      * Get key for Abema!
      */
     const abema = () => {
-        Object.defineProperty(__CLIENT_REGION__, 'isAllowed', {
-            get: () => true
+        Object.defineProperty(__CLIENT_REGION__, "isAllowed", {
+            get: () => true,
         });
-        Object.defineProperty(__CLIENT_REGION__, 'status', {
-            get: () => false
+        Object.defineProperty(__CLIENT_REGION__, "status", {
+            get: () => false,
         });
         const _Uint8Array = Uint8Array;
         Uint8Array = class extends _Uint8Array {
             constructor(...args) {
-                super(...args)
+                super(...args);
                 if (this.length === 16) {
-                    const key = Array.from(new _Uint8Array(this)).map(i => i.toString(16).length === 1 ? '0' + i.toString(16) : i.toString(16)).join('');
-                    if (key !== '00000000000000000000000000000000') {
+                    const key = Array.from(new _Uint8Array(this))
+                        .map((i) =>
+                            i.toString(16).length === 1
+                                ? "0" + i.toString(16)
+                                : i.toString(16)
+                        )
+                        .join("");
+                    if (key !== "00000000000000000000000000000000") {
                         chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                            "type": "key",
-                            "key": key
+                            type: "key",
+                            key: key,
                         });
                     }
                 }
                 return this;
             }
-        }
-    }
+        };
+    };
     const hibiki = (xhr) => {
-        if (xhr.readyState === 4 && xhr.responseURL.includes('datakey')) {
-            const key = Array.from(new Uint8Array(xhr.response)).map(i => i.toString(16).length === 1 ? '0' + i.toString(16) : i.toString(16)).join('');
+        if (xhr.readyState === 4 && xhr.responseURL.includes("datakey")) {
+            const key = Array.from(new Uint8Array(xhr.response))
+                .map((i) =>
+                    i.toString(16).length === 1
+                        ? "0" + i.toString(16)
+                        : i.toString(16)
+                )
+                .join("");
             chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                "type": "key",
-                "key": key
+                type: "key",
+                key: key,
             });
         }
-    }
+    };
     const nico = () => {
         try {
-            const liveData = JSON.parse(document.querySelector('#embedded-data').getAttribute('data-props'));
-            key = liveData.player.audienceToken;
+            const liveData = JSON.parse(
+                document
+                    .querySelector("#embedded-data")
+                    .getAttribute("data-props")
+            );
+            const websocketUrl = liveData.site.relive.webSocketUrl;
+            if (websocketUrl.match(/audience_token=(.+)/)[1]) {
+                key = websocketUrl.match(/audience_token=(.+)/)[1];
+            }
             chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                "type": "key",
-                "key": key
+                type: "key",
+                key: key,
             });
-        } catch {
-
-        }
-    }
+        } catch {}
+    };
 
     const nicocas = () => {
         chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-            "type": "cookies",
-            "cookies": 'user_session=' + document.cookie.match(/user_session\=(.+?)(;|$)/)[1]
+            type: "cookies",
+            cookies:
+                "user_session=" +
+                document.cookie.match(/user_session\=(.+?)(;|$)/)[1],
         });
         chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-            "type": "key",
-            "key": `<CAS_MODE, ID=${location.href.match(/(lv\d+)/)[1]}>`
+            type: "key",
+            key: `<CAS_MODE, ID=${location.href.match(/(lv\d+)/)[1]}>`,
         });
-    }
+    };
 
     const dmm = (xhr) => {
-        if (xhr.readyState === 4 && xhr.responseURL.startsWith('https://www.dmm.com/service/-/drm_iphone')) {
-            const key = Array.from(new Uint8Array(xhr.response)).map(i => i.toString(16).length === 1 ? '0' + i.toString(16) : i.toString(16)).join('');
+        if (
+            xhr.readyState === 4 &&
+            xhr.responseURL.startsWith(
+                "https://www.dmm.com/service/-/drm_iphone"
+            )
+        ) {
+            const key = Array.from(new Uint8Array(xhr.response))
+                .map((i) =>
+                    i.toString(16).length === 1
+                        ? "0" + i.toString(16)
+                        : i.toString(16)
+                )
+                .join("");
             chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                "type": "cookies",
-                "cookies": 'licenseUID=' + document.cookie.match(/licenseUID\=(.+?)(;|$)/)[1]
+                type: "cookies",
+                cookies:
+                    "licenseUID=" +
+                    document.cookie.match(/licenseUID\=(.+?)(;|$)/)[1],
             });
             chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                "type": "key",
-                "key": key
+                type: "key",
+                key: key,
             });
         }
-    }
+    };
 
     const dmm_r18 = (xhr) => {
-        if (xhr.readyState === 4 && xhr.responseURL.startsWith('https://www.dmm.co.jp/service/-/drm_iphone')) {
-            const key = Array.from(new Uint8Array(xhr.response)).map(i => i.toString(16).length === 1 ? '0' + i.toString(16) : i.toString(16)).join('');
+        if (
+            xhr.readyState === 4 &&
+            xhr.responseURL.startsWith(
+                "https://www.dmm.co.jp/service/-/drm_iphone"
+            )
+        ) {
+            const key = Array.from(new Uint8Array(xhr.response))
+                .map((i) =>
+                    i.toString(16).length === 1
+                        ? "0" + i.toString(16)
+                        : i.toString(16)
+                )
+                .join("");
             chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                "type": "cookies",
-                "cookies": 'licenseUID=' + document.cookie.match(/licenseUID\=(.+?)(;|$)/)[1]
+                type: "cookies",
+                cookies:
+                    "licenseUID=" +
+                    document.cookie.match(/licenseUID\=(.+?)(;|$)/)[1],
             });
             chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                "type": "key",
-                "key": key
+                type: "key",
+                key: key,
             });
         }
-    }
+    };
 
     const ch360 = (xhr) => {
         chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-            "type": "cookies",
-            "cookies": 'ch360pt=' + document.cookie.match(/ch360pt\=(.+?)(;|$)/)[1]
+            type: "cookies",
+            cookies:
+                "ch360pt=" + document.cookie.match(/ch360pt\=(.+?)(;|$)/)[1],
         });
-    }
+    };
 
     const twicas = async () => {
-        const userName = location.href.match(/https:\/\/twitcasting.tv\/(.+?)(\/|$)/);
+        const userName = location.href.match(
+            /https:\/\/twitcasting.tv\/(.+?)(\/|$)/
+        );
         if (userName) {
-            await fetch(`https://twitcasting.tv/${userName[1]}/metastream.m3u8`);
+            await fetch(
+                `https://twitcasting.tv/${userName[1]}/metastream.m3u8`
+            );
         }
-    }
+    };
 
     const youtube = async () => {
         const playerResponse = ytplayer.config.args.player_response;
         if (playerResponse) {
-            const HlsManifestUrl = JSON.parse(playerResponse).streamingData.hlsManifestUrl;
+            const HlsManifestUrl = JSON.parse(playerResponse).streamingData
+                .hlsManifestUrl;
             await fetch(HlsManifestUrl);
         }
         chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-            "type": "cookies",
-            "cookies": 'PREF=' + document.cookie.match(/PREF\=(.+?)(;|$)/)[1]
+            type: "cookies",
+            cookies: "PREF=" + document.cookie.match(/PREF\=(.+?)(;|$)/)[1],
         });
-    }
+    };
 
     const showroom = (xhr) => {
-        if (xhr.responseURL.includes('api/live/streaming_url')) {
+        if (xhr.responseURL.includes("api/live/streaming_url")) {
             const response = JSON.parse(xhr.responseText);
-            if (response.streaming_url_list.some(i => i.url.endsWith('chunklist.m3u8'))) {
+            if (
+                response.streaming_url_list.some((i) =>
+                    i.url.endsWith("chunklist.m3u8")
+                )
+            ) {
                 chrome.runtime.sendMessage(MINYAMI_EXTENSION_ID, {
-                    "type": "playlist_chunklist",
-                    "content": xhr.responseText,
-                    "url": xhr.responseURL,
-                    "title": document.title.replace(/[\/\*\\\:|\?<>]/ig, ""),
-                    "chunkLists": response.streaming_url_list.filter(i => i.url.endsWith('chunklist.m3u8')).map(i => {
-                        return {
-                            "type": "video",
-                            "bandwidth": i.quality * 1024,
-                            "resolution": {
-                                x: 'Unknown',
-                                y: 'Unknown'
-                            },
-                            url: i.url
-                        }
-                    })
+                    type: "playlist_chunklist",
+                    content: xhr.responseText,
+                    url: xhr.responseURL,
+                    title: document.title.replace(/[\/\*\\\:|\?<>]/gi, ""),
+                    chunkLists: response.streaming_url_list
+                        .filter((i) => i.url.endsWith("chunklist.m3u8"))
+                        .map((i) => {
+                            return {
+                                type: "video",
+                                bandwidth: i.quality * 1024,
+                                resolution: {
+                                    x: "Unknown",
+                                    y: "Unknown",
+                                },
+                                url: i.url,
+                            };
+                        }),
                 });
             }
         }
-    }
+    };
 
     // Execute when load
     switch (location.host) {
-        case 'abema.tv': {
+        case "abema.tv": {
             abema();
             break;
         }
-        case 'hibiki-radio.jp': {
+        case "hibiki-radio.jp": {
             hibiki();
             break;
         }
-        case 'www.dmm.com': {
+        case "www.dmm.com": {
             dmm();
             break;
         }
-        case 'www.dmm.co.jp': {
+        case "www.dmm.co.jp": {
             dmm_r18();
             break;
         }
-        case 'twitcasting.tv': {
+        case "twitcasting.tv": {
             twicas();
             break;
         }
-        case 'www.youtube.com': {
+        case "www.youtube.com": {
             youtube();
             break;
         }
     }
-})()
+})();
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,73 +1,92 @@
-const fs = require('fs');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const fs = require("fs");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const generateHTMLPluginConf = (input) => {
     const result = [];
     for (const file of Object.keys(input)) {
-        result.push(new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, `../dist/${file}/index.html`),
-            template: path.resolve(__dirname, '../src/index.html'),
-            chunks: ['vendors', input[file]['output']]
-        }))
+        result.push(
+            new HtmlWebpackPlugin({
+                filename: path.resolve(__dirname, `../dist/${file}/index.html`),
+                template: path.resolve(__dirname, "../src/index.html"),
+                chunks: ["vendors", input[file]["output"]],
+            })
+        );
     }
     return result;
-}
+};
 
 const generateEntries = (input) => {
     const result = {};
     for (const file of Object.keys(input)) {
-        result[input[file]['output']] = path.resolve(__dirname, `../src/${input[file]['input']}`);
+        result[input[file]["output"]] = path.resolve(
+            __dirname,
+            `../src/${input[file]["input"]}`
+        );
     }
     return result;
-}
+};
 
 const generateConfig = (input) => {
     return {
-        mode: 'production',
+        mode: "production",
         entry: generateEntries(input),
         resolve: {
-            extensions: ['.js', '.vue']
+            extensions: [".js", ".vue"],
         },
         plugins: [
             new CleanWebpackPlugin({
-                verbose: true
+                verbose: true,
             }),
-            new CopyWebpackPlugin([{
-                from: path.resolve(__dirname, '../assets/**/*'), to: path.resolve(__dirname, '../dist/'),
-            }]),
+            new CopyWebpackPlugin([
+                {
+                    from: path.resolve(__dirname, "../assets/**/*"),
+                    to: path.resolve(__dirname, "../dist/"),
+                },
+            ]),
             new ExtractTextPlugin("[name].css"),
             ...generateHTMLPluginConf(input),
             {
                 apply: (compiler) => {
-                    compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
-                        console.log('Write manifest');
-                        const manifest = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../manifest.json')));
-                        fs.writeFileSync(path.resolve(__dirname, '../dist/manifest.json'), JSON.stringify(manifest, null, 2))
+                    compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
+                        console.log("Write manifest");
+                        const manifest = JSON.parse(
+                            fs.readFileSync(
+                                path.resolve(__dirname, "../manifest.json")
+                            )
+                        );
+                        fs.writeFileSync(
+                            path.resolve(__dirname, "../dist/manifest.json"),
+                            JSON.stringify(manifest, null, 2)
+                        );
                     });
-                }
-            }
+                },
+            },
         ],
         module: {
-            rules: [{
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    extractCSS: true
-                }
-            }, {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            }, {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-            }]
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                    options: {
+                        extractCSS: true,
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    loader: "style-loader!css-loader",
+                },
+                {
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                    loader: "url-loader",
+                },
+            ],
         },
         output: {
-            filename: '[name].js',
+            filename: "[name].js",
             path: path.resolve(__dirname, `../dist/`),
         },
         optimization: {
@@ -76,25 +95,24 @@ const generateConfig = (input) => {
                     commons: {
                         test: /[\\/]node_modules[\\/]/,
                         name: "vendors",
-                        chunks: "all"
+                        chunks: "all",
                     },
-
                 },
-                name: true
-            }
+                name: true,
+            },
         },
-    }
+    };
 };
 
 module.exports = [
     generateConfig({
-        "background": {
-            input: 'pages/background/index.js',
-            output: 'background/bundle'
+        background: {
+            input: "pages/background/index.js",
+            output: "background/bundle",
         },
-        "config": {
-            input: 'pages/config/index.js',
-            output: 'config/bundle'
-        }
-    })
-]
+        config: {
+            input: "pages/config/index.js",
+            output: "config/bundle",
+        },
+    }),
+];
