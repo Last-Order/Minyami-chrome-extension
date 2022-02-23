38d45e0d0b9c37ddcf4c7d81497e16bd405fbad4
Eridanus Sora <sora@sound.moe>
2022-02-24 00:22:15 +0800

    fix status tip

--- src/messages/en.js
+++ src/messages/en.js
@@ -27,7 +27,7 @@ export default {
     },
     tooltip: {
         initial: "No data available",
-        waiting: "Waiting for streams...",
+        waiting: "Waiting for more data...",
         ready: "Streams available",
         stopped: "Page not supported"
     }
