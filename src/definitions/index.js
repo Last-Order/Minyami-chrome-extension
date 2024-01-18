10870f93d7395cf1a48193d4650c2c27c9d9f4ca
Eridanus Sora <sora@sound.moe>
2024-01-19 00:28:20 +0800

    support gsch

--- src/definitions/index.js
+++ src/definitions/index.js
@@ -4,7 +4,6 @@ export const supportedSites = [
     "live2.nicovideo.jp",
     "live.nicovideo.jp",
     "cas.nicovideo.jp",
-    "gyao.yahoo.co.jp",
     "www.dmm.com",
     "www.dmm.co.jp",
     "www.360ch.tv",
@@ -20,7 +19,8 @@ export const supportedSites = [
     "live.bilibili.com",
     "playervspf.channel.or.jp",
     "nicochannel.jp",
-    "pizzaradio.jp"
+    "pizzaradio.jp",
+    "gs-ch.com",
 ];
 export const needCookiesSites = ["360ch.tv"];
 export const needKeySites = [
@@ -33,32 +33,35 @@ export const needKeySites = [
     "www.onsen.ag",
     "nicochannel.jp",
     "pizzaradio.jp",
+    "gs-ch.com",
 ];
 export const siteAdditionalHeaders = {
     "www.openrec.tv": {
-        Referer: "https://www.openrec.tv/"
+        Referer: "https://www.openrec.tv/",
     },
     "www.onsen.ag": {
-        Referer: "https://www.onsen.ag/"
+        Referer: "https://www.onsen.ag/",
     },
     "twitcasting.tv": {
         Referer: "https://twitcasting.tv/",
-        "User-Agent": navigator.userAgent
-    }
+        "User-Agent": navigator.userAgent,
+    },
 };
 export const siteThreadsSettings = {
-    "twitcasting.tv": 3
+    "twitcasting.tv": 3,
 };
 export const minyamiVersionRequirementMap = {
     "live.nicovideo.jp": "4.2.5",
     "live.bilibili.com": "4.2.4",
     "twitcasting.tv": "4.2.10",
-    "nicochannel.jp": "4.4.13"
+    "nicochannel.jp": "4.4.13",
+    "pizzaradio.jp": "4.4.13",
+    "gs-ch.com": "4.4.13",
 };
 export const statusFlags = {
     supported: 0b1,
     missingCookie: 0b10,
-    missingKey: 0b100
+    missingKey: 0b100,
 };
 export const parseStatusFlags = (flags) => {
     const status = {};
